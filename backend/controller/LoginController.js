1579dcc121952524dbc0e27d613975a8923d84a8
kientc6811 <kientc6811@co-well.com.vn>
2018-03-20 11:11:38 +0700

    config folder

---
+++ backend/controller/LoginController.js
@@ -0,0 +1,6 @@
+var auth = {
+    index: function(req, res) {
+        res.render("index");
+    }
+}
+module.exports = home;
---
+++ server.js
@@ -0,0 +1,4 @@
+var express = require('express');
+var app = express();
+app.set('views', 'view');
+app.set('view engine', 'twig')
